c8ca9f69513ca0e3c0955e841ac9dd518f98fc09
jack3 <41743337+jack3-github@users.noreply.github.com>
2025-07-01 07:00:48 +0900

    first commit

---
+++ data.js
@@ -0,0 +1,18 @@
+// data.js
+// ここにフラッシュカードのデータを記述します。
+
+const flashcardsData = [
+    {
+        questionImage: './images/5add2.jpg',
+        answerText: '７',
+    },
+    {
+        questionImage: './images/5add7.jpg',
+        answerText: '１２',
+    },
+    {
+        questionImage: './images/6add7.jpg',
+        answerText: '１３',
+    },
+    // 必要に応じて、さらに問題を追加してください。
+];
