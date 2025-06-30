8e55dd9d0598f378de6aab24dc0224877cb95564
jack3 <41743337+jack3-github@users.noreply.github.com>
2025-07-01 07:41:28 +0900

    data add

--- data.js
+++ data.js
@@ -3,8 +3,16 @@
 
 const flashcardsData = [
     {
+        questionImage: './images/1add1.jpg',
+        answerText: '２',
+    },
+    {
         questionImage: './images/2add7.jpg',
         answerText: '９',
+    },
+    {
+        questionImage: './images/3add5.jpg',
+        answerText: '８',
     },
     {
         questionImage: './images/5add2.jpg',
@@ -17,6 +25,10 @@ const flashcardsData = [
     {
         questionImage: './images/6add7.jpg',
         answerText: '１３',
+    },
+    {
+        questionImage: './images/7add4.jpg',
+        answerText: '１１',
     },
     // 必要に応じて、さらに問題を追加してください。
 ];
