a37e3872f7c6bd4abc9a4ddcd2fdf3297e8baef3
jack3 <41743337+jack3-github@users.noreply.github.com>
2025-07-01 07:51:05 +0900

    data add

--- data.js
+++ data.js
@@ -7,8 +7,72 @@ const flashcardsData = [
         answerText: '２',
     },
     {
+        questionImage: './images/1add2.jpg',
+        answerText: '３',
+    },
+    {
+        questionImage: './images/1add3.jpg',
+        answerText: '４',
+    },
+    {
+        questionImage: './images/1add4.jpg',
+        answerText: '５',
+    },
+    {
+        questionImage: './images/1add5.jpg',
+        answerText: '６',
+    },
+    {
+        questionImage: './images/1add6.jpg',
+        answerText: '７',
+    },
+    {
+        questionImage: './images/1add7.jpg',
+        answerText: '８',
+    },
+    {
+        questionImage: './images/1add8.jpg',
+        answerText: '９',
+    },
+    {
+        questionImage: './images/1add9.jpg',
+        answerText: '１０',
+    },
+    {
+        questionImage: './images/2add1.jpg',
+        answerText: '３',
+    },
+    {
+        questionImage: './images/2add2.jpg',
+        answerText: '４',
+    },
+    {
+        questionImage: './images/2add3.jpg',
+        answerText: '５',
+    },
+    {
+        questionImage: './images/2add4.jpg',
+        answerText: '６',
+    },
+    {
+        questionImage: './images/2add5.jpg',
+        answerText: '７',
+    },
+    {
+        questionImage: './images/2add6.jpg',
+        answerText: '８',
+    },
+    {
         questionImage: './images/2add7.jpg',
         answerText: '９',
+    },
+    {
+        questionImage: './images/2add8.jpg',
+        answerText: '１０',
+    },
+    {
+        questionImage: './images/2add9.jpg',
+        answerText: '１１',
     },
     {
         questionImage: './images/3add5.jpg',
